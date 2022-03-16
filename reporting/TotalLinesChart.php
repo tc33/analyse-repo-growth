078e85c0073dc7656266cccbc13075dc0850c564
Tom Castle <tom@vibeagency.uk>
2022-03-16 18:24:54 +0000

    Generate chartjs code for total lines chart

---
+++ reporting/TotalLinesChart.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace TC33\AnalyseRepoGrowth;
+
+class TotalLinesChart implements Report {
+
+	public function generate(array $data) {
+		$versions    = $this->versions($data);
+		$totalCounts = array_column( array_column($data, 'SUM'), 'code' );
+
+		file_put_contents(Report::REPORTS_DIR . '/total-lines.chart.html', $this->chartSource($versions, $totalCounts));
+	}
+
+	private function versions(array $data): array {
+		return array_keys($data);
+	}
+
+	private function chartSource(array $versions, array $totalCounts) {
+		$versionsString    = "'" . implode("','", $versions) . "'";
+		$totalCountsString = implode(",", $totalCounts);
+
+		ob_start();
+		?>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js@3.7.1"></script>
+<canvas id="total-lines-chart" width="400" height="400"></canvas>
+<script>
+	var ctx = document.getElementById('total-lines-chart');
+	var myChart = new Chart(ctx, {
+		type:    'bar',
+		data:    {
+			labels:   [<?php echo $versionsString; ?>],
+			datasets: [{
+				label:           'Lines of Code',
+				data:            [<?php echo $totalCountsString; ?>],
+				backgroundColor: 'rgba(54, 162, 235, 0.2)',
+				borderColor:     'rgba(54, 162, 235, 1)',
+				borderWidth:     1
+			}]
+		},
+		options: {
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero: true
+					}
+				}]
+			}
+		}
+	});
+</script>
+
+		<?php
+		return ob_get_clean();
+	}
+}
--- reporting/generate-reports.php
+++ reporting/generate-reports.php
@@ -1,16 +1,18 @@
 <?php
 
 use TC33\AnalyseRepoGrowth\DataLoader;
+use TC33\AnalyseRepoGrowth\TotalLinesChart;
 
 include __DIR__ . '/DataLoader.php';
 include __DIR__ . '/Report.php';
 include __DIR__ . '/LinesByVersionReport.php';
+include __DIR__ . '/TotalLinesChart.php';
 include __DIR__ . '/CSVGenerator.php';
 
 $dataLoader = new DataLoader(__DIR__ . '/../data');
 $data       = $dataLoader->getData();
 
-$reports = [LinesByVersionReport::class];
+$reports = [LinesByVersionReport::class, TotalLinesChart::class];
 
 foreach ($reports as $reportType) {
 	(new $reportType)->generate($data);
