863bc75ae2bb3f58c30b78a32122ab3aca3547f8
Tom Castle <tom@vibeagency.uk>
2022-03-16 18:52:36 +0000

    Generate chartjs code for versions lines chart

---
+++ reporting/VersionsLinesChart.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace TC33\AnalyseRepoGrowth;
+
+class VersionsLinesChart implements Report {
+
+	const BG_COLOURS      = [
+		'rgba(255, 99, 132, 0.2)',
+		'rgba(255, 206, 86, 0.2)',
+		'rgba(75, 192, 192, 0.2)',
+		'rgba(153, 102, 255, 0.2)',
+		'rgba(255, 159, 64, 0.2)'
+	];
+	const BORDER_COLOURS  = ['rgba(255, 99, 132, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)'];
+	const OTHER_LANGUAGES = ['XML', 'SVG', 'Sass', 'Markdown', 'JSON'];
+
+	public function generate(array $data) {
+		$languages = array_diff($this->languages($data), self::OTHER_LANGUAGES, ['SUM']);
+		$versions  = $this->versions($data);
+		$counts    = [];
+
+		foreach ($languages as $language) {
+			$counts[$language] = array_column(array_column($data, $language), 'code');
+		}
+
+		$counts['Other'] = [];
+
+		foreach ($data as $version => $versionData) {
+			$count = 0;
+
+			foreach (self::OTHER_LANGUAGES as $language) {
+				$count += $versionData[$language]['code'] ?? 0;
+			}
+
+			$counts['Other'][] = $count;
+		}
+
+		file_put_contents(Report::REPORTS_DIR . '/versions-lines.chart.html', $this->chartSource($versions, $counts));
+	}
+
+	private function languages(array $data): array {
+		$languages = [];
+
+		foreach ($data as $versionData) {
+			$languages = array_unique(array_merge($languages, array_keys($versionData)));
+		}
+
+		return $languages;
+	}
+
+	private function versions(array $data): array {
+		return array_keys($data);
+	}
+
+	private function chartSource(array $versions, array $counts) {
+		$versionsString = "'" . implode("','", $versions) . "'";
+		$languages      = array_keys($counts);
+
+		ob_start();
+		?>
+
+		<script src="https://cdn.jsdelivr.net/npm/chart.js@3.7.1"></script>
+		<canvas id="versions-lines-chart" width="400" height="400"></canvas>
+		<script>
+			var ctx = document.getElementById('versions-lines-chart');
+			var myChart = new Chart(ctx, {
+				type:    'bar',
+				data:    {
+					labels:   [<?php echo $versionsString; ?>],
+					datasets: [
+						<?php foreach ($languages as $index => $language) : ?>
+						{
+							label:           '<?php echo $language; ?>',
+							data:        [<?php echo implode(",", $counts[$language]); ?>],
+							backgroundColor: '<?php echo self::BG_COLOURS[$index]; ?>',
+							borderColor:     '<?php echo self::BORDER_COLOURS[$index]; ?>',
+							borderWidth: 1
+						},
+						<?php endforeach; ?>
+					]
+				},
+				options: {
+					scales: {
+						x:     {
+							stacked: true
+						},
+						y:     {
+							stacked: true
+						},
+						yAxes: [{
+							ticks: {
+								beginAtZero: true
+							}
+						}]
+					}
+				}
+			});
+		</script>
+
+		<?php
+		return ob_get_clean();
+	}
+}
--- reporting/generate-reports.php
+++ reporting/generate-reports.php
@@ -2,17 +2,19 @@
 
 use TC33\AnalyseRepoGrowth\DataLoader;
 use TC33\AnalyseRepoGrowth\TotalLinesChart;
+use TC33\AnalyseRepoGrowth\VersionsLinesChart;
 
 include __DIR__ . '/DataLoader.php';
 include __DIR__ . '/Report.php';
 include __DIR__ . '/LinesByVersionReport.php';
 include __DIR__ . '/TotalLinesChart.php';
+include __DIR__ . '/VersionsLinesChart.php';
 include __DIR__ . '/CSVGenerator.php';
 
 $dataLoader = new DataLoader(__DIR__ . '/../data');
 $data       = $dataLoader->getData();
 
-$reports = [LinesByVersionReport::class, TotalLinesChart::class];
+$reports = [LinesByVersionReport::class, TotalLinesChart::class, VersionsLinesChart::class];
 
 foreach ($reports as $reportType) {
 	(new $reportType)->generate($data);
