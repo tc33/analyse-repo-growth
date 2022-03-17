2bc00d19e43fb5d43ffbf82c05289384b1ebf6a8
Tom Castle <tom@vibeagency.uk>
2022-03-17 17:48:47 +0000

    Save version dates to CSV

--- loc-wp.sh
+++ loc-wp.sh
@@ -3,10 +3,15 @@
 rm -rf wordpress
 [ -d "./data" ] && printf "Removing existing data directory\n"
 rm -rf data
+[ -d "./versions.csv" ] && printf "Removing existing versions file\n"
+rm versions.csv
 
 # Clone WordPress repo
 git clone git@github.com:WordPress/WordPress.git wordpress
 cd wordpress
+
+# Create empty versions CSV with headers
+echo "version,date" >> ../versions.csv
 
 # Get list of major versions matching pattern x.x
 versions=( $(git tag -l "[0-9].[0-9]") )
@@ -21,6 +26,11 @@ do
 
   # Perform count excluding default plugins and themes
   cloc --exclude-dir=.git --fullpath --not-match-d="wp-content/(plugins|themes)" --hide-rate --csv --report-file=../data/$i.csv .
+
+  # Get the date of this version
+  printf "Saving release date of $i\n"
+  release_date=$(git show -s --format=%ci)
+  echo "$i,$release_date" >> ../versions.csv
 done
 
 # Generate the reports
@@ -34,3 +44,5 @@ printf "Deleting WordPress\n"
 rm -rf wordpress
 printf "Deleting data files\n"
 rm -rf data
+#printf "Deleting versions file\n"
+#rm versions.csv
