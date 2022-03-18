21314af1b09ac1027c66b82fa0d45dcf1d5d20a0
Tom Castle <tom@vibeagency.uk>
2022-03-18 10:06:16 +0000

    Generate chart displayings lines of code by date

---
+++ reporting/LinesByDateChart.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace TC33\AnalyseRepoGrowth;
+
+use DateTime;
+
+class LinesByDateChart implements Report {
+
+	public function generate(array $data) {
+		$versions    = $this->versions($data);
+		$totalCounts = array_column(array_column($data, 'SUM'), 'code');
+		$totalCounts = array_combine($versions, $totalCounts);
+		$dates       = $this->versionDates();
+
+		file_put_contents(Report::REPORTS_DIR . '/lines-by-version-chart.html', $this->chartSource($dates, $totalCounts));
+	}
+
+	private function versions(array $data): array {
+		return array_keys($data);
+	}
+
+	private function versionDates(): array {
+		$versionsFile = fopen('../versions.csv', 'r');
+		$columns      = array_flip(fgetcsv($versionsFile, 0, ','));
+		$versionDates = [];
+
+		while (! feof($versionsFile)) {
+			$row = fgetcsv($versionsFile, 0, ',');
+
+			if (! $row) {
+				continue;
+			}
+
+			$version = $row[$columns['version']];
+			$date    = $row[$columns['date']];
+
+			$date = DateTime::createFromFormat('Y-m-d H:i:s O', $date);
+
+			$versionDates[$version] = $date->format('Y-m-d H:i:s');
+		}
+		fclose($versionsFile);
+
+		return $versionDates;
+	}
+
+	private function chartSource(array $dates, array $totalCounts) {
+		ob_start();
+		?>
+
+		<script src="https://cdn.jsdelivr.net/npm/chart.js@3.7.1"></script>
+		<script src="https://cdn.jsdelivr.net/npm/chartjs-adapter-date-fns/dist/chartjs-adapter-date-fns.bundle.min.js"></script>
+		<canvas id="lines-by-dates-chart" width="400" height="400"></canvas>
+		<script>
+			var ctx = document.getElementById('lines-by-dates-chart');
+			var myChart = new Chart(ctx, {
+				type:    'line',
+				data:    {
+					datasets: [{
+						label:           'Lines of code',
+						backgroundColor: 'rgba(54, 162, 235, 0.2)',
+						borderColor:     'rgba(54, 162, 235, 1)',
+						data:            [
+							<?php foreach ($totalCounts as $version => $count) : ?>
+							{
+								x: '<?php echo $dates[$version]; ?>',
+								y: <?php echo $count; ?>,
+							},
+							<?php endforeach; ?>
+						]
+					}]
+				},
+				options: {
+					scales: {
+						x: {
+							type: 'time'
+						}
+					}
+				}
+			});
+			const footer = (tooltipItems) => {
+				return 'Version: ';
+			};
+		</script>
+
+		<?php
+		return ob_get_clean();
+	}
+}
--- reporting/generate-reports.php
+++ reporting/generate-reports.php
@@ -1,6 +1,7 @@
 <?php
 
 use TC33\AnalyseRepoGrowth\DataLoader;
+use TC33\AnalyseRepoGrowth\LinesByDateChart;
 use TC33\AnalyseRepoGrowth\TotalLinesChart;
 use TC33\AnalyseRepoGrowth\VersionsLinesChart;
 
@@ -9,12 +10,13 @@ include __DIR__ . '/Report.php';
 include __DIR__ . '/LinesByVersionReport.php';
 include __DIR__ . '/TotalLinesChart.php';
 include __DIR__ . '/VersionsLinesChart.php';
+include __DIR__ . '/LinesByDateChart.php';
 include __DIR__ . '/CSVGenerator.php';
 
 $dataLoader = new DataLoader(__DIR__ . '/../data');
 $data       = $dataLoader->getData();
 
-$reports = [LinesByVersionReport::class, TotalLinesChart::class, VersionsLinesChart::class];
+$reports = [LinesByVersionReport::class, TotalLinesChart::class, VersionsLinesChart::class, LinesByDateChart::class];
 
 foreach ($reports as $reportType) {
 	(new $reportType)->generate($data);
